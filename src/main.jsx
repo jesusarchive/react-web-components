4c5f316879ce5263dc561f0294bc8bf303551c2c
jesusbro <jesusbro@ext.inditex.com>
2022-09-12 13:41:15 +0200

    add react-to-webcomponent, app as web component test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import reactToWebComponent from "react-to-webcomponent";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const MyApp = reactToWebComponent(App, React, ReactDOMClient);
+
+customElements.define("my-app", MyApp);
